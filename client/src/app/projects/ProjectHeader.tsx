c62ec516fdac5a8e70ed01f5f9a59d25be3f2673
jigsaw1270 <nomadshomoy@gmail.com>
2024-10-17 15:05:45 +0600

    .eslintrc fix

--- client/src/app/projects/ProjectHeader.tsx
+++ client/src/app/projects/ProjectHeader.tsx
@@ -2,22 +2,22 @@ import Header from '@/components/Header';
 import React, { useState } from 'react'
 
 type Props = {
-    activeTab: string;
-    setActiveTab: (tabName: string) => void;
-  };
+  activeTab: string;
+  setActiveTab: (tabName: string) => void;
+};
 
 const ProjectHeader = ({ activeTab, setActiveTab }: Props) => {
-    const [isModalNewProjectOpen ,setIsModalNewTaskOpen] = useState(false);
+  const [isModalNewProjectOpen, setIsModalNewProjectOpen] = useState(false);
   return (
     <div className='px-4 xl:px-6'>
-        {/* MODAL NEW project */}
-        <div className='pb-6 pt-6 lg:pb-4 lg:pt-8'>
-            <Header name='Projuct Design Development'/>
-
-        </div>
-
-    </div>
+      {/* MODAL NEW project */}
+      <div className='pb-6 pt-6 lg:pb-4 lg:pt-8'>
+        <Header name='Project Design Development' />
+      </div>
+     </div>
   )
 }
 
 export default ProjectHeader
+
+
--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -4,18 +4,18 @@ import ProjectHeader from "@/app/projects/ProjectHeader";
 
 
 type Props = {
-    params: { id: string};
+  params: { id: string };
 };
 
-const Project = ({params}: Props) => {
-    const { id } =  params;
-    const [ activeTab ,setActiveTab] = useState("Board");
-    const [ isModalNewTaskOpen , setIsModalNewTaskOpen] = useState(false);
+const Project = ({ params }: Props) => {
+  const { id } = params;
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
   return (
     <div>
-        {/* MODAL NEW TASK */}
-        <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-        {/* { activeTab === "Board" && ( <Board/>)} */}
+      {/* MODAL NEW TASK */}
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {/* { activeTab === "Board" && ( <Board/>)} */}
     </div>
   )
 }
