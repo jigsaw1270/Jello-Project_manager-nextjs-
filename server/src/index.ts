bb8e141dcaa02d92c0440cc83ba16bc803d55573
jigsaw1270 <nomadshomoy@gmail.com>
2024-11-20 00:47:19 +0600

    server modifications

--- server/src/index.ts
+++ server/src/index.ts
@@ -40,7 +40,7 @@ app.use("/teams",teamRoutes);
 
 //server 
 
-const port = process.env.PORT || 3000 ;
-app.listen(port, ()=> {
+const port = Number(process.env.PORT) || 3000 ;
+app.listen(port, "0.0.0.0", ()=> {
     console.log(`server running on port ${port}`);
 });
