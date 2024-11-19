ca6802aa2fb024485c7f807acaef30c2af2de6d0
jigsaw1270 <nomadshomoy@gmail.com>
2024-11-19 23:44:15 +0600

    home

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -65,12 +65,19 @@ export interface SearchResults {
     users?: User[];
 }
 
+export interface Team {
+    teamId : number ;
+    teamName : string;
+    projectOwnerUserId?: number;
+    projectManagerUserId?: number;
+}
+
 export const api =  createApi({
     baseQuery : fetchBaseQuery ({
         baseUrl :  process.env.NEXT_PUBLIC_API_BASE_URL
     }),
     reducerPath : 'api' ,
-    tagTypes : ["Projects" , "Tasks", "Users"],
+    tagTypes : ["Projects" , "Tasks", "Users","Teams"],
     endpoints : (build) => ({
         getProjects : build.query<Project[],void>({
             query: () => "projects",
@@ -110,6 +117,10 @@ export const api =  createApi({
             query: () => "users",
             providesTags:["Users"]
         }),
+        getTeams: build.query<Team[], void>({
+            query: ()=> "teams",
+            providesTags: ["Teams"],
+        }),
         search : build.query<SearchResults, string>({
             query : (query) => `search?query=${query}`,
 
@@ -118,4 +129,4 @@ export const api =  createApi({
 });
 
 
-export const { useGetProjectsQuery , useCreateProjectMutation ,useGetTasksQuery , useCreateTaskMutation, useUpdateTaskStatusMutation, useSearchQuery, useGetUsersQuery}  = api;
+export const { useGetProjectsQuery , useCreateProjectMutation ,useGetTasksQuery , useCreateTaskMutation, useUpdateTaskStatusMutation, useSearchQuery, useGetUsersQuery, useGetTeamsQuery}  = api;
